1519f0360a60ae766cf90206b9cb78a4e1ee6098
Gavin L. Simpson <gavin.simpson@ucl.ac.uk>
2012-04-05 05:38:57 +0000

    version 0.2-0

--- R/corAxis.R
+++ R/corAxis.R
@@ -1,15 +1,13 @@
-"corAxis" <-
-function(x, ...)
-  UseMethod("corAxis")
+`corAxis` <- function(x, ...)
+    UseMethod("corAxis")
 
-"corAxis.default" <-
-function(x, ...)
-  stop("No default method for corAxis")
+`corAxis.default` <- function(x, ...)
+    stop("No default method for corAxis")
 
-"corAxis.symcoca" <-
-function(x, axes = c(1:min(6, x$n.axes)), ...) {
-  if (class(x) != "symcoca")
-    stop("object must be of class \"symcoca\"")
-  score.dat <- scores(x, axes, display = "site")
-  diag(cor(score.dat$X1, score.dat$X2))
+`corAxis.symcoca` <- function(x, axes = c(1:min(6, x$n.axes)),
+                              ...) {
+    if (class(x) != "symcoca")
+        stop("object must be of class \"symcoca\"")
+    scrs <- scores(x, axes, display = "sites")
+    diag(cor(scrs$sites$Y, scrs$sites$X))
 }
