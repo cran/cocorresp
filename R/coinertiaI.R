5b709120b50bd39044c097e0b9d56c8fe522e013
Gavin L. Simpson <ucfagls@gmail.com>
2014-11-19 20:45:58 +0000

    version 0.2-3

--- R/coinertiaI.R
+++ R/coinertiaI.R
@@ -1,21 +1,18 @@
-"coinertiaI" <-
-function(X, Y, fast = TRUE)
-  {
+`coinertiaI` <- function(X, Y, fast = TRUE) {
     A <- t(X) %*% Y
-    if (fast) {
-      svdA <- La.svd(A, nu = 0)
-      Psi <- Y %*% t(svdA$vt)
-      return(Psi)
+    retval <- if (fast) {
+        svdA <- La.svd(A, nu = 0)
+        Psi <- Y %*% t(svdA$vt)
     } else {
-      svdA <- La.svd(A)
-      Ksi <- X %*% svdA$u
-      Psi <- Y %*% t(svdA$vt)
-      L <- diag(svdA$d)^2
-      retval <- list(weights = list(X = svdA$u, Y = t(svdA$vt)),
-                     scores = list(X = Ksi, Y = Psi),
-                     lambda = L, call = match.call())
-      class(retval) <- c("coinertiaI", "fitCoinertia")
-      return(retval)
+        svdA <- La.svd(A)
+        Ksi <- X %*% svdA$u
+        Psi <- Y %*% t(svdA$vt)
+        L <- diag(svdA$d)^2
+        retval <- list(weights = list(X = svdA$u, Y = t(svdA$vt)),
+                       scores = list(X = Ksi, Y = Psi),
+                       lambda = L, call = match.call())
+        class(retval) <- c("coinertiaI", "fitCoinertia")
     }
-  }
+    retval
+}
 
