70a01e8f1f677c23e55e0978122fb823ae388ef3
Gavin L. Simpson <ucfagls@gmail.com>
2019-12-19 08:20:03 +0000

    version 0.4-1

--- inst/doc/cocorresp-intro.R
+++ inst/doc/cocorresp-intro.R
@@ -2,27 +2,27 @@
 knitr::opts_chunk$set(message = FALSE, warning = FALSE)
 library("cocorresp")
 
-## ----load-data-----------------------------------------------------------
+## ----load-data----------------------------------------------------------------
 data(beetles)
 ## log transform the beetle data
 beetles <- log1p(beetles)
 data(plants)
 
-## ----fit-symcoca, message = TRUE-----------------------------------------
+## ----fit-symcoca, message = TRUE----------------------------------------------
 bp.sym <- coca(beetles ~ ., data = plants, method = "symmetric")
 
-## ----data-process, message = TRUE----------------------------------------
+## ----data-process, message = TRUE---------------------------------------------
 beetles <- beetles[, colSums(beetles) > 0]
 plants <- plants[, colSums(plants) > 0]
 bp.sym <- coca(beetles ~ ., data = plants, method = "symmetric")
 
-## ----print-symcoca-------------------------------------------------------
+## ----print-symcoca------------------------------------------------------------
 bp.sym
 
-## ----screeplot-symcoca---------------------------------------------------
+## ----screeplot-symcoca--------------------------------------------------------
 screeplot(bp.sym)
 
-## ----plot-symcoca, fig.width = 14, fig.height = 7, fig.show = "hold"-----
+## ----plot-symcoca, fig.width = 14, fig.height = 7, fig.show = "hold"----------
 layout(matrix(1:2, ncol = 2))
 biplot(bp.sym, which = "y1", main = "Beetles")
 biplot(bp.sym, which = "y2", main = "Plants")
